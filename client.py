f739a6ac94159eb002d16c0910bdc7bff4f840c6
Pratik Bhangale <pratik@localhost.localdomain>
2017-11-11 17:53:10 +0530

    Done

--- client.py
+++ client.py
@@ -28,6 +28,7 @@ def chat_client():
     s.send(var);
     buff = []
     buff = s.recv(4096);
+    print buff
     if buff == 'Authenticated':
         print 'Connected to remote host. You can start sending messages'
         with open('DATA/'+user+'.txt',"r+") as f:
@@ -36,6 +37,9 @@ def chat_client():
                 print data
             f.write('')
             f.truncate(0)
+    else:
+    	s.close()
+    	return 
     while 1 :
         socket_list = [sys.stdin, s]
         ready_to_read,ready_to_write,in_error = select.select(socket_list , [], [])
@@ -48,35 +52,6 @@ def chat_client():
                 # user entered a message
                 msg = sys.stdin.readline()
                 s.send(msg)
-                # sys.stdout.write('[Me] '); sys.stdout.flush() 
-    # while 1:
-    #     socket_list = [sys.stdin, s]
-         
-    #     # Get the list sockets which are readable
-    #     ready_to_read,ready_to_write,in_error = select.select(socket_list , [], [])
-         
-    #     for sock in ready_to_read:             
-    #         if sock == s:
-    #             # incoming message from remote server, s
-    #             var = raw_input("Enter 'Username:Password' - ")
-    #             var = str(var);
-    #             sock.send(var);
-
-                # data = sock.recv(4096)
-                # if not data :
-                #     print '\nDisconnected from chat server'
-                #     sys.exit()
-                # else :
-                #     #print data
-                #     sys.stdout.write(data)
-                #     sys.stdout.write('[Me] '); sys.stdout.flush()     
-            
-            # else :
-            #     # user entered a message
-            #     msg = sys.stdin.readline()
-            #     s.send(msg)
-            #     sys.stdout.write('[Me] '); sys.stdout.flush() 
-
 if __name__ == "__main__":
 
     sys.exit(chat_client())
--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 import sys
 import socket
 import select
-
+import time
 HOST = '' 
 SOCKET_LIST = []
 RECV_BUFFER = 4096 
-PORT = 9009
+PORT = 9010
 def chat_server():
 	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -15,17 +15,32 @@ def chat_server():
 	with open('DATA/auth.txt') as f:
 		auth = f.readlines()
 	auth = [x.strip() for x in auth]
+	blocked_list = {}
+	for a in auth:
+		us = a.split(":")[0]
+		blocked_list[us] = []
+	connection_info = {}
 	online_user = {}
 	online_sock = {}
+	block_ip = {}
+	logged_out = {}
 	while 1:
 		ready_to_read,ready_to_write,in_error = select.select(SOCKET_LIST,[],[],0)
+
 		for sock in ready_to_read:
 			if sock == server_socket: 
 				sockfd, addr = server_socket.accept()
+				if addr[0] in block_ip:
+					if time.time()-block_ip[addr[0]] < 60:
+						sockfd.send("IP has been blocked");
+						continue
 				buff = []
 				buff = sockfd.recv(4096)
 				print buff
 				username = buff.split(":")[0]
+				if username in online_user:
+					sockfd.send("User already logged in")
+					continue
 				flag = 0;
 				for j in auth:
 					if j == buff:
@@ -37,6 +52,18 @@ def chat_server():
 					online_sock[sockfd] = username
 					print "Client (%s, %s) connected" % addr
 					sockfd.send("Authenticated");
+				else:
+					if addr[0] in connection_info:
+						connection_info[addr[0]] += 1
+						if connection_info[addr[0]] >=3:
+							print "blocked %s" %addr[0]
+							block_ip[addr[0]] = time.time()
+					else:
+						connection_info[addr[0]] = 1
+					if connection_info[addr[0]] > 0:
+						sockfd.send("Please check username and password, You have "+str(3 - connection_info[addr[0]])  +" attempts left");
+					else:
+						sockfd.send("Your IP has been blocked for 60 seconds")
 			else:
 				# process data recieved from client, 
 				try:
@@ -51,9 +78,15 @@ def chat_server():
 							message = senduser + ':' + ' '.join(parse[1:])
 							for s in SOCKET_LIST:
 								if s != server_socket and s != sock:
+									recvuser = online_sock[s]
+									if senduser in blocked_list[recvuser]:
+										continue
 									s.send(message)
 						elif command1 == 'message':
 							recvuser = parse[1]
+							print recvuser, senduser ,blocked_list
+							if senduser in blocked_list[recvuser]:
+								continue
 							message = senduser + ':' + ' '.join(parse[2:])
 							if online_user.has_key(recvuser):
 								usersock = online_user[recvuser]
@@ -68,13 +101,44 @@ def chat_server():
 									users.append(key)
 							message = 'Server:'+','.join(users)
 							sock.send(message)
-						# there is something in the socket
-						# broadcast(server_socket, sock, "\r" + '[' + str(sock.getpeername()) + '] ' + data)  
+						elif command2 == 'wholasthr':
+							users = []
+							for us in online_user:
+								users.append(us)
+							for us in logged_out:
+								if time.time()-logged_out[us] <= 3600 and us not in users :
+										users.append(us)
+								else:
+									logged_out.pop(us,None)
+							message = 'Server:'+','.join(users)
+							sock.send(message)
+						elif command1 == 'block':
+							usr = parse[1].split("\n")[0]
+							blocked_list[senduser].append(usr);
+							sock.send("Server : Blocked "+usr)
+							print blocked_list
+						elif command1 == 'unblock':
+							usr = parse[1].split("\n")[0]
+							blocked_list[senduser].remove(usr);
+							sock.send("Server : Unblocked "+usr)
+						else command2 == 'logout':
+							if sock in SOCKET_LIST:
+								SOCKET_LIST.remove(sock)
+								temp_user = online_sock[sock]
+								online_sock.pop(sock)
+								online_user.pop(temp_user)
+								logged_out[temp_user] = time.time()
+						else:
+							message = 'Server: Please Enter Valid Command'
+							sock.send(message)
 					else:
 						# remove the socket that's broken    
 						if sock in SOCKET_LIST:
 							SOCKET_LIST.remove(sock)
-
+							temp_user = online_sock[sock]
+							online_sock.pop(sock)
+							online_user.pop(temp_user)
+							logged_out[temp_user] = time.time()
 						# at this stage, no data means probably the connection has been broken
 						# broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr) 
 
